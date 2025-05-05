cfa7abfb23c81a9ada572069a3ff405dbedc3126
Vyacheslav <sovuix@gmail.com>
2025-05-05 22:51:40 +0300

    create 09

---
+++ architect/09/src/application.js
@@ -0,0 +1,47 @@
+import keyBy from 'lodash/keyBy.js'
+import has from 'lodash/has.js'
+import isEmpty from 'lodash/isEmpty.js'
+import * as yup from 'yup'
+// import { proxy, subscribe, snapshot } from 'valtio/vanilla'
+import axios from 'axios'
+
+// urls нельзя хардкодить: https://ru.hexlet.io/blog/posts/izbavlyaytes-ot-strok
+const routes = {
+    usersPath: () => '/users',
+}
+
+const schema = yup.object().shape({
+    name: yup.string().trim().required(),
+    email: yup.string().required('email must be a valid email').email(),
+    password: yup.string().required().min(6),
+    passwordConfirmation: yup.string()
+        .required('password confirmation is a required field')
+        .oneOf(
+            [yup.ref('password'), null],
+            'password confirmation does not match to password',
+        ),
+})
+
+// Этот объект можно использовать для того, чтобы обрабатывать ошибки сети.
+// Это необязательное задание, но крайне рекомендуем попрактиковаться.
+const errorMessages = {
+    network: {
+        error: 'Network Problems. Try again.',
+    },
+}
+
+// Используйте эту функцию для выполнения валидации
+// Выведите в консоль её результат, чтобы увидеть, как получить сообщения об ошибках
+const validate = (fields) => {
+    try {
+        schema.validateSync(fields, { abortEarly: false })
+        return {}
+    }
+    catch (e) {
+        return keyBy(e.inner, 'path')
+    }
+}
+
+// BEGIN (write your solution here)
+
+// END
---
+++ architect/09/src/index.js
@@ -0,0 +1,7 @@
+// @ts-check
+
+// import 'core-js/stable/index.js'
+// import 'regenerator-runtime/runtime.js'
+import app from './application.js'
+
+app()
